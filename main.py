c89b24dd24b685988d7674b3b5a3baf8c469a925
philipkarev <filipp.karev@math.msu.ru>
2022-11-11 22:48:12 +0300

    Added some changes. Final commit. Task completed.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ def define_matrix(SInputFile):  # заполнение массива числа
 
     a = []
     isf = 0
-
-    with open(SInputFile) as f:
-        while True:
-            try:
+    try:
+        with open(SInputFile) as f:
+            while True:
                 s = f.readline()  # считываем символ
 
                 if not s:  # выходим, если конец
@@ -16,19 +15,40 @@ def define_matrix(SInputFile):  # заполнение массива числа
                 if isf == 0:
                     n = int(s[0])
                     isf += 1
-                else:
-                    for i in range(0, len(s)):
-                        a.append(int(s[i]))
 
-            except ValueError:
-                print("Error: bad value.")
-                return [-1, n]
-            except FileNotFoundError:
-                print("Error: file not found.")
-                return [-1, n]
+                for i in range(0, len(s)):
+                    if isf == 1:
+                        isf += 1
+                        continue
+                    a.append(int(s[i]))
+
+    except ValueError:
+        print("Error: bad value.")
+        f.close()
+        return [-1, 0]
+
+    except FileNotFoundError:
+        print("Error: file not found.")
+        return [-1, 0]
+
+    if isf == 0:
+        print("Error: file is empty.")
+        return[-1, 0]
+
+    if n <= 0:
+        print("Error: n <= 0.")
+        return [-1, n]
+
+    if len(a) == 0:
+        print("Error: matrix is empty.")
+        return [-1, n]
 
     if (len(a) % (n * n)) != 0:
         print("Error: matrix not squared.")
+        return [-1, n]
+
+    if n * n != len(a):
+        print("Error: no correct data - n * n != len(a)")
         return [-1, n]
 
     return [a, n]
@@ -46,22 +66,10 @@ def print_matrix(a, n):
 
 def change_last_and_first(a, n):
 
-    for i in range(n):  # дублирую первую строку матр.
-        a.append(a[i])
-
-    # print_matrix(a, n)
-
-    j = 0
-    for i in range(n * n - n, n * n):  # вставляю значения предпоследней строки в первую
-        a[j] = a[i]
-        j += 1
-
-    # print_matrix(a, n)
-
-    for i in range(n * n - 1, n * n - n - 1, -1):  # удаляю последнюю строку
-        a.pop(i)
-
-    # print_matrix(a, n)
+    for i in range(n):
+        tmp = a[i]
+        a[i] = a[n * n - n + i]
+        a[n * n - n + i] = tmp
 
     return a
 
@@ -71,9 +79,7 @@ def main():
 
     [arr, n] = define_matrix("1.txt")
 
-    if isinstance(arr, int):
-        print("Error: no correct data.")
-    else:
+    if not isinstance(arr, int):
         print("-------------------")
         print("The original array:")
         print_matrix(arr, n)
